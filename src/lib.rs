1a1ad0093235571215b28df3cc3cb265cd012861
qiaogaojia <qiaogaojian@vip.qq.com>
2024-03-05 12:38:22 +0800

    feat: 斐波那契数列计算

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,30 @@ fn greet(name: &str) -> PyResult<String> {
     Ok(format!("Hello, {}!", name))
 }
 
+#[pyfunction]
+fn get_fibonacci(number: isize) -> PyResult<u128> {
+    if number == 1 {
+        return Ok(1);
+    } else if number == 2 {
+        return Ok(2);
+    }
+
+    let mut sum = 0;
+    let mut last = 0;
+    let mut curr = 1;
+    for _ in 1..number {
+        sum = last + curr;
+        last = curr;
+        curr = sum;
+    }
+    Ok(sum)
+}
+
 #[pymodule]
 fn rustcore(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(say_hello_to_python, m)?)?;
     m.add_function(wrap_pyfunction!(greet, m)?)?;
+    m.add_function(wrap_pyfunction!(get_fibonacci, m)?)?;
+
     Ok(())
 }
