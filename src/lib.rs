ee03e7bdeca6ac89a29e0e8159b73db55051ead6
qiaogaojia <qiaogaojian@vip.qq.com>
2024-03-05 10:24:27 +0800

    feat: 带参数方法

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,14 @@ fn say_hello_to_python() -> PyResult<String> {
     Ok("Hello, Python!".to_string())
 }
 
+#[pyfunction]
+fn greet(name: &str) -> PyResult<String> {
+    Ok(format!("Hello, {}!", name))
+}
+
 #[pymodule]
 fn rustcore(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(say_hello_to_python, m)?)?;
+    m.add_function(wrap_pyfunction!(greet, m)?)?;
     Ok(())
 }
